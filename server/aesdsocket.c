52e4fc482680069b3b19d1472eb02e5fe96125cd
Sidd Jove <siddjove@gmail.com>
2025-12-23 01:20:07 +0530

    Fix aesdsocket implementation (thread-safe, correct file IO)

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <signal.h>
-#include <fcntl.h>
 #include <pthread.h>
+#include <fcntl.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
 #include <syslog.h>
 
 #define PORT 9000
 #define DATA_FILE "/var/tmp/aesdsocketdata"
+#define BACKLOG 10
 #define BUF_SIZE 1024
 
 static volatile sig_atomic_t keepRunning = 1;
@@ -30,76 +31,43 @@ static void signal_handler(int signo)
     keepRunning = 0;
 }
 
-static int server_init(void)
+void *processClientData(void *arg)
 {
-    int sockfd;
-    struct sockaddr_in addr;
-    int opt = 1;
+    struct clientInfo *client = (struct clientInfo *)arg;
+    char buffer[BUF_SIZE];
+    char packet[BUF_SIZE];
+    int packet_len = 0;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        syslog(LOG_ERR, "socket failed");
-        return -1;
-    }
-
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        syslog(LOG_ERR, "bind failed");
-        close(sockfd);
-        return -1;
-    }
-
-    if (listen(sockfd, 10) < 0) {
-        syslog(LOG_ERR, "listen failed");
-        close(sockfd);
-        return -1;
-    }
-
-    return sockfd;
-}
-
-static void *processClientData(void *arg)
-{
-    struct clientInfo *client = arg;
-    char recvbuf[BUF_SIZE];
-    char accum[BUF_SIZE * 10];
-    size_t accum_len = 0;
+    syslog(LOG_INFO, "Accepted connection from %s",
+           inet_ntoa(client->clientAddr.sin_addr));
 
     while (keepRunning) {
-        ssize_t r = recv(client->clientFD, recvbuf, sizeof(recvbuf), 0);
-        if (r <= 0) break;
+        ssize_t bytes = recv(client->clientFD, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) {
+            break;
+        }
 
-        memcpy(accum + accum_len, recvbuf, r);
-        accum_len += r;
+        for (ssize_t i = 0; i < bytes; i++) {
+            packet[packet_len++] = buffer[i];
 
-        char *newline;
-        while ((newline = memchr(accum, '\n', accum_len)) != NULL) {
-            size_t line_len = newline - accum + 1;
+            if (buffer[i] == '\n') {
+                pthread_mutex_lock(&file_mutex);
 
-            pthread_mutex_lock(&file_mutex);
+                int fd = open(DATA_FILE, O_CREAT | O_RDWR | O_APPEND, 0644);
+                write(fd, packet, packet_len);
+                lseek(fd, 0, SEEK_SET);
 
-            int fd = open(DATA_FILE, O_CREAT | O_WRONLY | O_APPEND, 0644);
-            write(fd, accum, line_len);
-            close(fd);
+                char sendbuf[BUF_SIZE];
+                ssize_t r;
+                while ((r = read(fd, sendbuf, sizeof(sendbuf))) > 0) {
+                    send(client->clientFD, sendbuf, r, 0);
+                }
 
-            fd = open(DATA_FILE, O_RDONLY);
-            char sendbuf[BUF_SIZE];
-            ssize_t s;
-            while ((s = read(fd, sendbuf, sizeof(sendbuf))) > 0) {
-                send(client->clientFD, sendbuf, s, 0);
+                close(fd);
+                pthread_mutex_unlock(&file_mutex);
+
+                packet_len = 0;
             }
-            close(fd);
-
-            pthread_mutex_unlock(&file_mutex);
-
-            memmove(accum, accum + line_len, accum_len - line_len);
-            accum_len -= line_len;
         }
     }
 
@@ -117,23 +85,36 @@ int main(int argc, char *argv[])
         if (opt == 'd') daemonize = 1;
     }
 
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+
     struct sigaction sa = {0};
     sa.sa_handler = signal_handler;
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    openlog("aesdsocket", LOG_PID, LOG_USER);
+    int sockFD = socket(AF_INET, SOCK_STREAM, 0);
+    int optval = 1;
+    setsockopt(sockFD, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
-    int sockfd = server_init();
-    if (sockfd < 0) exit(EXIT_FAILURE);
+    struct sockaddr_in serverAddr = {0};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(sockFD, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    listen(sockFD, BACKLOG);
 
     if (daemonize) {
-        if (fork() > 0) exit(0);
+        pid_t pid = fork();
+        if (pid > 0) exit(0);
         setsid();
         chdir("/");
         close(STDIN_FILENO);
         close(STDOUT_FILENO);
         close(STDERR_FILENO);
+        open("/dev/null", O_RDWR);
+        dup(0);
+        dup(0);
     }
 
     remove(DATA_FILE);
@@ -142,23 +123,20 @@ int main(int argc, char *argv[])
         struct clientInfo *client = malloc(sizeof(*client));
         socklen_t len = sizeof(client->clientAddr);
 
-        client->clientFD = accept(sockfd,
-            (struct sockaddr *)&client->clientAddr, &len);
-
+        client->clientFD = accept(sockFD,
+                                  (struct sockaddr *)&client->clientAddr,
+                                  &len);
         if (client->clientFD < 0) {
             free(client);
             continue;
         }
-
-        syslog(LOG_INFO, "Accepted connection from %s",
-               inet_ntoa(client->clientAddr.sin_addr));
 
         pthread_t tid;
         pthread_create(&tid, NULL, processClientData, client);
         pthread_detach(tid);
     }
 
-    close(sockfd);
+    close(sockFD);
     remove(DATA_FILE);
     closelog();
     return 0;
